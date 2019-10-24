0a143a1ac2ea86d92b17c08a6e7bd51eccc7cd1f
fuad2019 <56104356+fuad2019@users.noreply.github.com>
2019-10-24 12:54:41 +0600

    updated db
    updated db

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var getConnection = function(callback){
 	  host     : 'localhost',
 	  user     : 'root',
 	  password : '',
-	  database : 'node1'
+	  database : 'labfinal'
 	});
 	 
 	connection.connect(function(err) {
